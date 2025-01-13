bfee879172538a8d7af88eb9285725747d003a97
MADHAV <madhavmahajan152@gmail.com>
2025-01-13 12:23:30 +0530

    added post project to userRouter

--- server/Route/userRoute.js
+++ server/Route/userRoute.js
@@ -1,4 +1,10 @@
 import express from "express"
+import { postProject } from "../Controllers/userControllers.js";
 
 export const userRouter =express.Router();
+
+userRouter.post("/api/post-projects",upload.fields([
+    { name: 'image', maxCount: 1 },
+    { name: 'pdf', maxCount: 1 },
+]),postProject);
 
