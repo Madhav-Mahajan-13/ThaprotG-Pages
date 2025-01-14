63ce6cae1ecdf903d50af4f17c709dd43a1ec4cf
Indra <Indramohankhatri@gmail.com>
2025-01-14 18:25:27 +0530

    added some changes

--- client2/src/App.jsx
+++ client2/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
