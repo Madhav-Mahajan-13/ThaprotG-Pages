c2133220f5e00824ac3571cdf9a5bbc13d6e7a6d
Indra <ikhatri_be22@thapar.edu>
2025-01-18 22:36:34 +0530

    cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom';
-import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Landing from './pages/Landing';
@@ -9,7 +8,6 @@ import StudentProject from './pages/StudentProject';
 import AlumAchievement from './pages/AlumAchievement';
 import AlumProject from './pages/AlumProject';
 import CampusGallery from './pages/CampusGallery';
-import NavBar from './components/NavBar';
 import SignIn from './pages/login';
 import SignUp from './pages/register';
 import Forgot from './pages/forgot';
