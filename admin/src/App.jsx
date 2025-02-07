91f7fc617352235979659a12a97a411ff05c3250
Indra <ikhatri_be22@thapar.edu>
2025-02-08 02:33:26 +0530

    added some pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
+import {createBrowserRouter,Route,RouterProvider} from 'react-router-dom';
 import './App.css'
+import LoginSignup from './pages/loginSignup';
+import Landing from './pages/landing';
+
+const router = createBrowserRouter([
+  {
+    path:"/login",
+    element:<LoginSignup/>
+  },
+  {
+    path:"/",
+    element:<Landing/>
+  }
+])
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='font-bold'>HELLO WORLD</h1>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ admin/src/pages/landing.jsx
@@ -0,0 +1,7 @@
+export default function Landing(){
+    return(
+        <div className="flex">
+            <h1>THIS IS LANDING PAGE</h1>
+        </div>
+    )
+}
