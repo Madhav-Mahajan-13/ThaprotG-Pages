af5677dc0cfab7ade1d6b889149d727e8aabd708
MADHAV <madhavmahajan152@gmail.com>
2025-02-09 01:29:16 +0530

    ADMIN CREATION

---
+++ server/Controllers_admin/AdminCreationController.js
@@ -0,0 +1,10 @@
+import db from "../dbConnection.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const veiwAllSubAdmin = async(req,res)=>{}
+export const createSubAdmim = async(req,res)=>{}
+export const suspendSubAdmin = async(req,res)=>{}
+export const activeSubAdmin = async(req,res)=>{}
+
