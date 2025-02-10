cb4bc13dd057686b9918963e784142193dea91d6
MADHAV <madhavmahajan152@gmail.com>
2025-02-11 00:54:29 +0530

    suspended user check

--- server/Route/projectsRoute.js
+++ server/Route/projectsRoute.js
@@ -1,8 +1,17 @@
 import express from "express";
+import { getAlumProjects, searchProjects } from "../Controllers/alumProjectController.js";
+import { getStudentProjects } from "../Controllers/studentProjectController.js";
+import { uploadProjectFiles } from "../middleware/uploadFiles.js";
+import { getUserProjects, postProject } from "../Controllers/userProjectController.js";
 
 const projectRouter = express.Router();
 
 projectRouter.get();
 
 
-projectRouter.post();
+projectRouter.post("/allproject",getAlumProjects);
+projectRouter.post("/search",searchProjects);
+projectRouter.post("/allproject",getStudentProjects);
+// projectRouter.post("/search",searchStudentProjects)
+projectRouter.post("/postPrject",uploadProjectFiles,postProject)
+projectRouter.post("/getUserProject",getUserProjects)
