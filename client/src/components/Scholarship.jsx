b77d1be9e23f69c2a0e97a8fbd81c1f579d5a57b
MADHAV <madhavmahajan152@gmail.com>
2025-02-12 16:16:25 +0530

    changes

--- client/src/components/Scholarship.jsx
+++ client/src/components/Scholarship.jsx
@@ -4,7 +4,7 @@ import "../styling/Scholarship.css"
 const Scholarship = () => {
   return (
     <div className="scholarship-container">
-      {["Merit Based", "Need Based", "Sports", "Cultural","Merit Based", "Need Based", "Sports", "Cultural"].map((type) => (
+      {["Merit Based", "Need Based", "Sports", "Cultural","Merit Based", "Need Based", "Sports", "Cultural","Sports"].map((type) => (
         <div key={type} className="scholarship-card">
           <h3>{type} Scholarship</h3>
           <p>Financial support for deserving students.</p>
